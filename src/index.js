7f41d2e407592e54f12dce4f0926e4e12471c255
IshiAki0314made <127808195+IshiAki0314made@users.noreply.github.com>
2023-03-14 15:52:03 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// const val4 = {
+//   name:"じゃけ",
+//   age:28
+// };
+
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+console.log(val5);
